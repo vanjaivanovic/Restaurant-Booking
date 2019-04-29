023d83354cdec5f2e3238da52ef765308c452632
Vanja Ivanovic <vanja.i@hotmail.com>
2019-04-29 20:25:15 +0200

    fix router

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,25 @@ import '../App.css';
 import { Booking } from './booking/booking.js';
 import AdminPage from './admin/admin.js';
 import {
-  BrowserRouter as Router,
+  Switch,
+  BrowserRouter,
   Route
 } from 'react-router-dom';
 
 function App() {
   return(
-     <Router>
-        <div>
-            <Route path="/" component={StartPage} exact/>
-            <Route path="/AdminPage" component={AdminPage} />
-            <Route path="/Booking" component={Booking} />
-            <Route path="/Contact" component={Contact} /> 
-          </div>
-      </Router>
+     <div>
+       <BrowserRouter>
+        <Switch>
+          <div>
+              <Route path="/" component={StartPage} exact/>
+              <Route path="/AdminPage" component={AdminPage} />
+              <Route path="/Booking" component={Booking} />
+              <Route path="/Contact" component={Contact} /> 
+            </div>
+          </Switch>
+        </BrowserRouter>
+      </div>
   );
 }
 
