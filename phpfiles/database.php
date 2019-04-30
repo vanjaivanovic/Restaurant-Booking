171731ad06f8f32cefae74be0f4cc2867a389787
Vanja Ivanovic <vanja.i@hotmail.com>
2019-04-30 17:56:57 +0200

    Updates

--- phpfiles/database.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header("content-type:application/json");
-
-$connect = mysqli_connect("127.0.0.1", "root", "root", "restaurant");
